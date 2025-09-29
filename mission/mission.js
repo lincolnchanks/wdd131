804134de072d139e1b7c67c970989f26bb445eb7
lincolnchanks <lchanks2024@gmail.com>
2025-09-29 12:27:40 -0600

    Finally Finished Prove

--- mission/mission.js
+++ mission/mission.js
@@ -12,9 +12,13 @@ function changeTheme() {
     if (current == "dark") {
         body.style.backgroundColor = "#222";
         body.style.color = "#FFF"
+        logo.setAttribute('src', "images/dark-byui-logo.png")
         // logo.innerHTML = <img src="images/dark-byui-logo.png"></img>
         // code for changes to colors and logo
     } else {
+        body.style.backgroundColor = "#FFF"
+        body.style.color = "black"
+        logo.setAttribute("src", "images/byui-logo.png")
         // code for changes to colors and logo
     }
 }
