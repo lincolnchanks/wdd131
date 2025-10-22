318ba6ab534f3052172cacd6de1fb0d07f42ba62
lincolnchanks <lchanks2024@gmail.com>
2025-10-22 12:48:27 -0600

    W06 Prove AND Ponder

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,7 +1,7 @@
-const gallery = document.querySelector('#pics');
+const gallery = document.querySelector('.pics');
 const modal = document.querySelector('dialog');
-const modalImage = document.querySelector('img');
-const closeButton = document.querySelector('.close-viewer');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
 
 gallery.addEventListener('click', openModal);
 
@@ -11,11 +11,31 @@ function openModal(e) {
 
     const src = img.getAttribute('src');
     const alt = img.getAttribute('alt');
-
     const full = src.replace('sm', 'full');
 
     modalImage.src = full;
     modalImage.alt = alt;
 
     modal.showModal();
+}
+
+closeButton.addEventListener('click', () => {
+    modal.close();
+    // console.log("Button");
+})
+
+modal.addEventListener('click', (event) => {
+    if (event.target === 'modal') {
+        modal.close();
+        // console.log("Clicked out of img");
+    }
+})
+
+let menuBtn = document.querySelector('.menu-btn');
+let navigation = document.querySelector('nav');
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    // navigation.classList.toggle("hide");
 }
