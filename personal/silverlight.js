e41387b3fcdcf3499addb04d997f1f18537394ac
lincolnchanks <lchanks2024@gmail.com>
2025-12-10 13:02:45 -0700

    Personal Project Progess 12/10/2025

--- personal/silverlight.js
+++ personal/silverlight.js
@@ -134,12 +134,42 @@ const characters = [
 ]
 
 let characterCardContainer = document.querySelector("#character-cards");
+let searchBtn = document.querySelector("#search-btn");
+
+searchBtn.addEventListener("click", search);
+
+function search(){
+    let characterQuery = document.querySelector("#search-input").value;
+    console.log(characterQuery);
+
+    // return an array of characters, filtering only for those whose names match the query.
+    let filterCharacters = characters.filter(function(character){
+        return (character.name.toLowerCase().includes(characterQuery.toLowerCase()));
+    });
+    
+    // DEBUG: PRINT ALL FILTERED CHARACTERS
+    // filterCharacters.forEach(function(character){
+    //     console.log(character.name);
+    // });
+
+    // TO-DO: Add a sort function.
+
+    // Clear out character card screen.
+    characterCardContainer.innerHTML = "";
+
+    // Render screen with new characters.
+    filterCharacters.forEach(function(character){
+        renderCharacterCard(character);
+    });
+}
 
 function getCharacterCardTemplate(character){
     return `<div class="char-card">
                 <h2>${character.name}</h2>
-                <img src="${character.imgSrc}" alt="Picture of Hoid">
-                <p>${character.descPreview}...</p>
+                <div class="char-img-container">
+                    <img src="${character.imgSrc}" alt="Picture of ${character.name}">
+                </div>
+                <p>${character.descPreview}...<a href="character.html">Read more</a></p>
             </div>`
 }
 
